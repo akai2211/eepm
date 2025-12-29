7f6d7e26b45e0cef93b9ab1291f5e810c127686e
Vitaly Lipatov <lav@etersoft.ru>
2025-12-29 14:50:17 +0300

    epm repack dbeaver-ce: remove non-Linux JNA libs and java-openjdk dependency

--- repack.d/dbeaver-ce.sh
+++ repack.d/dbeaver-ce.sh
@@ -7,6 +7,13 @@ PRODUCTCUR=dbeaver
 
 . $(dirname $0)/common.sh
 
+# Remove JNA libraries for non-Linux platforms (they create false dependencies like libc.so.1/7/8)
+for i in aix darwin dragonflybsd freebsd openbsd sunos win32 ; do
+    for dir in $BUILDROOT/usr/share/dbeaver-ce/plugins/com.sun.jna_*/com/sun/jna/$i* ; do
+        [ -d "$dir" ] && remove_dir "${dir#$BUILDROOT}"
+    done
+done
+
 move_to_opt
 rm usr/bin/$PRODUCT
 add_bin_link_command $PRODUCT $PRODUCTDIR/$PRODUCTCUR
@@ -17,5 +24,3 @@ fix_desktop_file "/usr/share/dbeaver-ce/dbeaver.png"
 fix_desktop_file "/usr/share/dbeaver-ce/" "$PRODUCTDIR/"
 
 install_file .$PRODUCTDIR/dbeaver.png /usr/share/pixmaps/dbeaver.png
-
-add_requires java-openjdk
