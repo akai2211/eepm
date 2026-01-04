b1df4cfed4eb88c9ac7afeeab0b56413da16e873
Vitaly Lipatov <lav@etersoft.ru>
2026-01-04 06:12:11 +0300

    epm play sunshine: switch from Fedora COPR to GitHub releases

--- play.d/sunshine.sh
+++ play.d/sunshine.sh
@@ -4,27 +4,27 @@ PKGNAME=Sunshine
 SUPPORTEDARCHES="x86_64 aarch64"
 VERSION="$2"
 DESCRIPTION="Self-hosted game stream host for Moonlight"
-URL="https://app.lizardbyte.dev/Sunshine"
+TIPS="Run epm play Sunshine=<version> to install specific version (e.g. 2025.924.154138)."
+URL="https://github.com/LizardByte/Sunshine"
 
 . $(dirname $0)/common.sh
-
-warn_version_is_not_supported
 
 is_openssl_enough 3 || fatal "There is no needed OpenSSL 3 in the system."
 
 arch=$(epm print info -a)
 
-# TODO: common way to install from Fedora copr
-# use API instead of HTML page (Anubis bot protection blocks scraping)
-BUILD_ID=$(epm tool json --get-json-value "https://copr.fedorainfracloud.org/api_3/build/list?ownername=lizardbyte&projectname=stable&packagename=Sunshine&status=succeeded" '["items",0,"id"]')
-
-PKGURL=$(eget --list --latest https://download.copr.fedorainfracloud.org/results/lizardbyte/stable/fedora-41-$arch/0$BUILD_ID-Sunshine/ "Sunshine-*.$arch.rpm")
+# Use GitHub releases (Fedora 41 packages work for ALT via repack)
+if [ "$VERSION" != "*" ] ; then
+    PKGURL="https://github.com/LizardByte/Sunshine/releases/download/v$VERSION/Sunshine-$VERSION-1.fc41.$arch.rpm"
+else
+    PKGURL=$(eget --list --latest https://github.com/LizardByte/Sunshine/releases "Sunshine-*-1.fc41.$arch.rpm")
+fi
 
 install_pkgurl
 
-cat <<EOF
+cat <<'EOF'
 
 Note: run
-# setcap cap_sys_admin+p $(readlink -f $(command -v sunshine))
+# setcap cap_sys_admin+p /usr/bin/sunshine
 to enable permissions for KMS capture (Capture of most Wayland-based desktop environments will fail unless this step is performed.)
 EOF
