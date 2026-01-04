080845b9f3f165958bd605b240eaf0dc90426179
Vitaly Lipatov <lav@etersoft.ru>
2026-01-04 06:27:00 +0300

    epm play onescript-engine: switch to GitHub, add pack.d for 2.0+

--- play.d/onescript-engine.sh
+++ play.d/onescript-engine.sh
@@ -4,28 +4,29 @@ PKGNAME=onescript-engine
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
 DESCRIPTION="OneScript Engine from the official site"
-URL="https://oscript.io/"
+TIPS="Run epm play onescript-engine=<version> to install specific version (e.g. 2.0.0 or 1.9.3)."
+URL="https://github.com/EvilBeaver/OneScript"
 
 . $(dirname $0)/common.sh
 
-warn_version_is_not_supported
-VERSION=1.9.3
+if [ "$VERSION" = "*" ] ; then
+    VERSION=$(get_github_tag "https://github.com/EvilBeaver/OneScript/")
+fi
 
-pkgtype=$(epm print info -p)
-case $pkgtype in
-    rpm)
-        PKGURL="https://oscript.io/downloads/latest/x64/onescript-engine-${VERSION}-1.fc26.noarch.rpm"
-        PKGMASK="onescript-engine-*.noarch.rpm"
-        ;;
-    *)
-        PKGURL="https://oscript.io/downloads/latest/x64/onescript-engine_${VERSION}_all.deb"
-        PKGMASK="onescript-engine_*_all.deb"
-        ;;
-esac
-
-#if [ "$VERSION" = "*" ] ; then
-    # can't parse from the page
-    # PKGURL="$(eget --list --latest https://oscript.io/downloads "$PKGMASK")"
-#fi
-
-install_pkgurl
+# linux-x64.zip (self-contained) appeared in 2.0.0
+# for older versions use deb/rpm from GitHub
+if [ "$(epm print compare "$VERSION" 2.0.0)" != "-1" ] ; then
+    PKGURL="https://github.com/EvilBeaver/OneScript/releases/download/v$VERSION/OneScript-$VERSION-linux-x64.zip"
+    install_pack_pkgurl
+else
+    pkgtype=$(epm print info -p)
+    case $pkgtype in
+        rpm)
+            PKGURL="https://github.com/EvilBeaver/OneScript/releases/download/v$VERSION/onescript-engine-$VERSION-1.fc26.noarch.rpm"
+            ;;
+        *)
+            PKGURL="https://github.com/EvilBeaver/OneScript/releases/download/v$VERSION/onescript-engine_${VERSION}_all.deb"
+            ;;
+    esac
+    install_pkgurl
+fi
