f6d59a66e607d3a4714b597bbf60055ffb7fb41e
Vitaly Lipatov <lav@etersoft.ru>
2026-01-08 15:19:33 +0300

    epm play spravki-bk: add SHA256 checksum verification

--- play.d/spravki-bk.sh
+++ play.d/spravki-bk.sh
@@ -10,7 +10,11 @@ URL="http://www.kremlin.ru/structure/additional/12/"
 
 warn_version_is_not_supported
 
+# WARNING: kremlin.ru does not support HTTPS, checksum verification is critical
 # Get download URL for Astra Linux version from kremlin.ru
 PKGURL=$(eget -q -O- http://www.kremlin.ru/structure/additional/12/ | grep -B1 "для Astra Linux" | grep -o 'href="[^"]*"' | sed 's/href="//;s/"//' | head -n 2 | tail -n 1)
 
-install_pack_pkgurl
+# Checksum for version 3.0.4 (2025-04-01) - update when new version is released
+PKGSUM="sha256:241fe546e22360b003bdc922f1e40d12bd1b87a01ce256d8b1739419718b6475"
+
+install_pack_pkgurl "" "$PKGSUM"
