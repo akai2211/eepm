3600776f0693f073f44218747e06693cc3d4ff7b
Vitaly Lipatov <lav@etersoft.ru>
2026-01-09 22:14:10 +0300

    release_commit.sh: add docmd for verbose output, add build steps
    - Add docmd prefix to show commands being executed
    - Add builder SSH call for Korinf build
    - Add epm repo pkgupdate for XimperLinux Additives

--- release_commit.sh
+++ release_commit.sh
@@ -25,14 +25,20 @@ minorversion=$(echo "$baseversion" | sed -e 's|.*\.||')
 
 [ "$((minorversion%2))" = 1 ] && version="$version-beta"
 
-./pack_in_onefile.sh || exit
+docmd ./pack_in_onefile.sh || exit
 
-git add packed
-git commit packed -m "commit packed $version"
+docmd git add packed
+docmd git commit packed -m "commit packed $version"
 
-push_tag $version
-gpush pub.github
+docmd push_tag $version
+docmd gpush pub.github
 
 
-rpmpub /var/ftp/pub/Korinf/sources
+docmd rpmpub /var/ftp/pub/Korinf/sources
 #rpmpub /var/ftp/pub/Etersoft/EPM/$baseversion/sources
+
+docmd ssh builder@builder /srv/builder/Projects/korinf/bin-common/eepm.sh x86_64/ALTLinux/Sisyphus || exit
+
+docmd epm repo pkgupdate /var/ftp/pub/Etersoft/XimperLinux/Current/Additives /var/ftp/pub/Korinf/x86_64/ALTLinux/Sisyphus/eepm-*.rpm
+#docmd epm repo index /var/ftp/pub/Etersoft/XimperLinux/Current/Additives 
+/home/lav/Projects/XimperLinux/repo-scripts/additives-genbasedir-sign.sh /var/ftp/pub/Etersoft/XimperLinux/Current/Additives
