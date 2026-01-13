bc683507735b31a97e464297b4116f7f550a64ef
Vitaly Lipatov <lav@etersoft.ru>
2026-01-13 20:54:49 +0300

    epm play qsp-classic: convert ~ to - in URL for pre-release versions

--- play.d/qsp-classic.sh
+++ play.d/qsp-classic.sh
@@ -3,6 +3,7 @@
 PKGNAME=QSP_Classic
 SUPPORTEDARCHES="x86_64"
 VERSION="$2"
+RELEASE="$3"
 DESCRIPTION='Classic QSP player'
 URL="https://github.com/QSPFoundation/qspgui/releases"
 
@@ -12,7 +13,11 @@ arch=x86_64
 if [ "$VERSION" = "*" ] ; then
     PKGURL=$(get_github_url "https://github.com/QSPFoundation/qspgui/" "${PKGNAME}-${VERSION}-$arch.AppImage")
 else
-    PKGURL="https://github.com/QSPFoundation/qspgui/releases/download/v$VERSION/${PKGNAME}-${VERSION}-$arch.AppImage"
+    # allow use 5.x.x-beta format
+    [ -z "$CHECKED_VERSION" ] && [ "$RELEASE" != "*" ] && VERSION="$VERSION-$RELEASE"
+    # replace ~ with - for URL (VERSION may contain ~b1 for pre-release)
+    URLVERSION="$(echo "$VERSION" | tr '~' '-')"
+    PKGURL="https://github.com/QSPFoundation/qspgui/releases/download/v$URLVERSION/${PKGNAME}-$URLVERSION-$arch.AppImage"
 fi
 
 install_pkgurl
