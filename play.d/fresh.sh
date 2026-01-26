2b2789874735e69a23a9b61e589352a6c30d3f8b
Vitaly Lipatov <lav@etersoft.ru>
2026-01-26 10:32:25 +0300

    epm play fresh: fix play script

--- play.d/fresh.sh
+++ play.d/fresh.sh
@@ -9,7 +9,6 @@ URL="https://github.com/sinelaw/fresh"
 
 . $(dirname $0)/common.sh
 
-arch="$(epm print info --distro-arch)"
 pkgtype="$(epm print info -p)"
 
 case "$pkgtype" in
@@ -22,13 +21,10 @@ esac
 
 # https://github.com/sinelaw/fresh/releases/download/v0.1.88/fresh-editor-0.1.88-1.x86_64.rpm
 # https://github.com/sinelaw/fresh/releases/download/v0.1.88/fresh-editor_0.1.88-1_amd64.deb
-case "$pkgtype" in
-    rpm)
-        PKGURL="$(get_github_url sinelaw/fresh fresh-editor-${VERSION}-${RELEASE}.$arch.rpm)"
-        ;;
-    deb)
-        PKGURL="$(get_github_url sinelaw/fresh fresh-editor_${VERSION}-${RELEASE}_$arch.deb)"
-        ;;
-esac
+if [ "$VERSION" = "*" ] || [ "$RELEASE" = "*" ] ; then
+    PKGURL="$(get_github_url sinelaw/fresh "$(epm print constructname $PKGNAME "$VERSION-*")")"
+else
+    PKGURL="https://github.com/sinelaw/fresh/releases/download/v$VERSION/$(epm print constructname $PKGNAME "$VERSION-$RELEASE")"
+fi
 
 install_pkgurl
